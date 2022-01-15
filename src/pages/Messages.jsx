e1492b11e53b336ed75f28de046d43145a587fea
sava <savamedvedev@gmail.com>
2022-01-15 22:32:30 +0800

    first-commit

---
+++ src/pages/Messages.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Messages = () => {
+  return (
+    <div className="messages">
+      <h1>Messages</h1>
+    </div>
+  );
+};
+
+export const MessagesOne = () => {
+  return (
+    <div className="messages">
+      <h1>Messages1</h1>
+    </div>
+  );
+};
+
+export const MessagesTwo = () => {
+  return (
+    <div className="messages">
+      <h1>Messages2</h1>
+    </div>
+  );
+};
---
+++ src/pages/Reports.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const Reports = () => {
+  return (
+    <div className="reports">
+      <h1>Reports</h1>
+    </div>
+  );
+};
+
+export const ReportsOne = () => {
+  return (
+    <div className="reports">
+      <h1>Reports1</h1>
+    </div>
+  );
+};
+
+export const ReportsTwo = () => {
+  return (
+    <div className="reports">
+      <h1>Reports2</h1>
+    </div>
+  );
+};
+
+export const ReportsThree = () => {
+  return (
+    <div className="reports">
+      <h1>Reports3</h1>
+    </div>
+  );
+};
